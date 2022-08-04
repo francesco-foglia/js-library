cf106fa6398e3f7aad417628d06c679e3c2acd40
Francesco Foglia <francescofogliajj@gmail.com>
2022-08-04 14:55:31 +0200

    App container

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+
+  const books = [
+    {
+      title: 'Il vecchio e il mare',
+      author: 'Ernest Hemingway',
+      year: '1951'
+    },
+    {
+      title: 'La forma dell\'acqua',
+      author: 'Andrea Camilleri',
+      year: '1994'
+    },
+    {
+      title: 'Elogio della fuga',
+      author: 'Henri Laborit',
+      year: '1976'
+    },
+    {
+      title: 'La camera azzurra',
+      author: 'Georges Simenon',
+      year: '1964'
+    }
+  ]
+
+});
